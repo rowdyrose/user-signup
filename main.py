354b7e61ce75f9957320e2b4f526abb8e9c5247e
Tessa Sanders <sanders.tessa@gmail.com>
2019-07-14 16:04:09 -0500

    started adding validation

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-from flask import Flask, request, redirect, render_template
-import cgi
+from flask import Flask, request, render_template
 import os
+import cgi
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/", methods=['post', 'get'])
+@app.route("/")
 def index():
     return render_template('form.html')
+def empty_value(val):
+    if val:
+        return True
+    else:
+        return False
+
+def char_length(val):
+    if len(val) > 4 and len(val) < 20:
+        return True
+    else:
+        return False
+
+def email_symbol(val):
+    if val.count('@') <= 1 and x.count('.') >= 1:
+        return True
+    else:
+        return False
+
+
 
 @app.route("/signup_complete", methods=['post'])
 def signup_complete():
