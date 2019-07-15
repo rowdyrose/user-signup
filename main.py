078588b7aadef84e80576c85d70919d3d36d2eaf
Tessa Sanders <sanders.tessa@gmail.com>
2019-07-14 21:46:04 -0500

    added validations to user signup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, render_template
-import os
+from flask import Flask, request, render_template, redirect, url_for
 import cgi
+import os
+import re
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -8,29 +10,55 @@ app.config['DEBUG'] = True
 @app.route("/")
 def index():
     return render_template('form.html')
-def empty_value(val):
-    if val:
-        return True
+
+@app.route("/sign_up", methods=['POST'])
+def sign_up():
+    username = request.form["username"]
+    password = ["password"]
+    verify_password = ["verify_password"]
+    email = ["email"]
+
+    username_error = ""
+    password_error = ""
+    verify_password_error = ""
+    email_error = ""
+
+# validate user name
+    if username =="":
+        username_error = "Please enter a valid username"
+    elif len(username) <=4 or len(username) > 14:
+        username_error = "Please enter a username that is between 4 and 12 characters in length"
+        username = ""
+    elif " " in username:
+        username_error = "Please enter a user name with out spaces"
+        username = ""
+
+#validate  password
+    if verify_password =="" or verify_password != password:
+        verify_password_error = "Your password fields do not macth, please re-enter"
+        verify_password = ""
+
+#validate user email
+    #only validating if something is entered in this field
+    if email != "":
+
+
+
+# if there are no errors, return the welcome message
+    if not username_error and not password_error and not verify_password_error and not email_error:
+        username = username
+        return render_template('welcome_page.html', username=username)
     else:
-        return False
+        return render_template('form.html', 
+        username = username, 
+        username_error = username_error, 
+        password_error = password_error, 
+        email = email, 
+        email_error = email_error)
 
-def char_length(val):
-    if len(val) > 4 and len(val) < 20:
-        return True
-    else:
-        return False
-
-def email_symbol(val):
-    if val.count('@') <= 1 and x.count('.') >= 1:
-        return True
-    else:
-        return False
-
-
-
-@app.route("/signup_complete", methods=['post'])
+@app.route('/welcome_page')
 def signup_complete():
-    username = request.form['username']
-    return render_template('welcome_page.html', name=username)
+    username = request.args.get('username')
+    return render_template('welcome_page.html', username=username)
 
 app.run()
