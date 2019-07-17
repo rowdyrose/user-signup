d464cbf3a2ce3b7a30915dc8dfc1062f42558e1c
Tessa <sanders.tessa@gmail.com>
2019-07-16 20:38:05 -0500

    attempted to fix ther validation

--- main.py
+++ main.py
@@ -1,50 +1,63 @@
 from flask import Flask, request, render_template, redirect
 import cgi
 import os
-from functions import *
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 # validate user name
 def validate_user(username):
-    
-    if len(username) <=4 or len(username) > 12 or username =="":   
+    username = ["username"]
+    if len(username) <= 4 or len(username) > 12 or username =="":   
         username_error = "Please enter a valid username that is between 4 and 12 characters in length"
-        return False
+        return username_error
     elif " " in username:
         username_error = "Please enter a user name with out spaces"
-        return False
+        return username_error
 
 #validate  password
-def password(password):
-    password_error = "please enter a valid password"
-    if password == "" or password <= 3 or password > 12:
+def validate_password(password):
+    password = ["password"]
+    password_error = "please enter a valid password between 4 to 12 characvters in length"
+    if password == "" or len(password) <= 3 or len(password) > 12:
         return password_error
 # validate password verify input
 
 def validate_verifypass(verifypass):
+    verifypass = ["verifypass"]
+    password = ["password"]
     verifypass_error = "Your password fields do not match, please re-enter"
     if verifypass != password:
         return verifypass_error
 
 #validate user email
     #only validating if something is entered in this field
-   # if email != "":
+#def val
+        #email = ["email"]
+        #if email != "":
+                #return True
+       #  elif email == 
 
 
 
 # if there are no errors, return the welcome message
 
-@app.route("/", methods=['post', 'get'])
+@app.route("/", methods=['get'])
 def index():
     return render_template('form.html')
 
 
 @app.route("/signup_complete", methods=['post'])
 def signup_complete():
+    
     username = request.form['username']
-    password()
+    password = request.form[ 'password']
+    verifypass = request.form['verifypass']
+    #email = request.form['email']
+    
+    validate_user(username)
+    validate_password(password)
+    validate_verifypass(verifypass)
     return render_template('welcome_page.html', name=username)
 
 app.run()
