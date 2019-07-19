2a57363b76018254c9cac8a86b8ef4e7a8f9410e
Tessa <sanders.tessa@gmail.com>
2019-07-18 20:20:04 -0500

    added to validations

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
-
 # validate user name
 def validate_user(username):
     if len(username) < 3 or len(username) > 20 or username =="":   
-        username_error = "Please enter a valid username that is between 4 and 12 characters in length"
+        username_error = "Please enter a valid username that is between 3 and 20 characters in length"
         return username_error
     elif " " in username:
         username_error = "Please enter a user name with out spaces"
@@ -28,25 +27,50 @@ def validate_verifypass(password, verifypass):
     verifypass_error = "Your password fields do not match, please re-enter"
     if verifypass != password:
         return verifypass_error
-    else
-        return 
-
 
 
 #validate user email
-    #only validating if something is entered in this field
-def validate_email
-        if email != "":
-                #return True
-       #  elif email == 
+  
+def validate_email(email):
+    email_error = "please enter a valid email address"
+        # if email contains more than one @ and more than one . , return error message
+    if email == " " or len(email) < 3 or len(email) > 20:
+        return email_error
+   
+    at_symbol = "@"
+    at_symbol_count = email.count(at_symbol)
+    
+    if at_symbol_count != 1:
+        return email_error
+
+    period = "."
+    period_count = email.count(period)
+    
+    if period_count != 1:
+        return email_error
+                     
 
 
 
-# if there are no errors, return the welcome message
 
-@app.route("/", methods=['POSTef index():
+@app.route("/", methods=['GET'])
+def index():
     return render_template('form.html')
 
+@app.route("/", methods=['POST'])
+def validate():
+
+    username = request.form["username"]
+    password = request.form["password"]
+    verifypass = request.form["verifypass"]
+    email = request.form["email"]
+    
+    if email != "":
+            validate_email(email)
+                
+    return render_template('form.html')
+
+# if there are no errors, return the welcome message
 
 @app.route("/signup_complete", methods=['post'])
 def signup_complete():
