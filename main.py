95d84a5801d4a2c54f7abab5f9ebb1cd9c6c42e3
Tessa Sanders <sanders.tessa@gmail.com>
2019-07-20 20:56:06 -0500

    fixed username validations

--- main.py
+++ main.py
@@ -16,42 +16,43 @@ def validate_user(username):
         username_error = "Please enter a user name with out spaces"
         return username_error
 
-#validate  password
-#def validate_password(password):
-#    password_error = ""
-#    if password == "" or len(password) < 3 or len(password) > 20:
-#        password_error = "please enter a valid password between 3 to 20 characvters in length"
-#        return password_error
+# validate  password
+def validate_password(password):
+    password_error = ""
+    if password == "" or len(password) < 3 or len(password) > 20:
+        password_error = "please enter a valid password between 3 to 20 characvters in length"
+        return password_error
 
 # validate password verify input
-#def validate_verifypass(password, verifypass):
-#    verifypass_error = ""
-#    if verifypass != password:
-#        verifypass_error = "Your password fields do not match, please re-enter"
-#        return verifypass_error
+def validate_verifypass(password, verifypass):
+    verifypass_error = ""
+    if verifypass != password:
+        verifypass_error = "Your password fields do not match, please re-enter"
+        return verifypass_error
 
 
 #validate user email
-#def validate_email(email):
-#    email_error = ""
-        # if email contains more than one @ and more than one . , return error message
-#    if email == " " or len(email) < 3 or len(email) > 20:
-#        email_error = "please enter a valid email address"
-#        return email_error
-   
-#    at_symbol = "@"
-#    at_symbol_count = email.count(at_symbol)
-    
-#    if at_symbol_count != 1:
-#        email_error = "please enter a valid email address"
-#        return email_error
+def validate_email(email):
+    email_error = ""
 
-#    period = "."
-#    period_count = email.count(period)
+# if email contains more than one @ and more than one . , return error message
+    if email == " " or len(email) < 3 or len(email) > 20:
+        email_error = "please enter a valid email address"
+        return email_error
+  
+    at_symbol = "@"
+    at_symbol_count = email.count(at_symbol)
    
-#    if period_count != 1:
-#        email_error = "please enter a valid email address"
-#        return email_error
+    if at_symbol_count != 1:
+        email_error = "please enter a valid email address"
+        return email_error
+
+    period = "."
+    period_count = email.count(period)
+   
+    if period_count != 1:
+        email_error = "please enter a valid email address"
+        return email_error
                      
 
 
@@ -65,24 +66,34 @@ def index():
 def validate():
 
     username = request.form["username"]
-#    password = request.form["password"]
-#    verifypass = request.form["verifypass"]
-#    email = request.form["email"]
+    password = request.form["password"]
+    verifypass = request.form["verifypass"]
+    email = request.form["email"]
     
     user_error = validate_user(username)
+    pass_error = validate_password(password)
+    verify_error = validate_verifypass(password, verifypass)
+    email_error_error = validate_email(email)
 
-#    if email != "":
-#        validate_email(email)
+
 
     if user_error:
-
         return render_template('index.html', user_error_placeholder=user_error)
+    
+    if pass_error:
+        return render_template('index.html', password_error_placeholder=pass_error)
+    
+    if verify_error:
+        return render_template('index.html', verifypass_error_placeholder=verify_error)
+
+    if email != "":
+        validate_email(email)
+
+        if email_error_error:
+            return render_template('index.html', email_error_placeholder=email_error_error)
 
 # if there are no errors, return the welcome message
-
-@app.route("/signup_complete", methods=['post'])
-def signup_complete():
-
-    return render_template('welcome_page.html', name=username)
+    else:
+        return render_template('welcome_page.html', name=username)
 
 app.run()
